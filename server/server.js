7e6cb91bc1b314286263f678a431304ee7b51316
Ilyas Mohamed <zswaleh@ymail.com>
2023-04-09 01:37:53 -0500

    server side boiler is set up

---
+++ server/server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const app = express();
+const PORT = 5000;
+
+app.listen(PORT, () => {
+    console.log ('Server is running on port', PORT)
+  })
